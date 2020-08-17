7003ff47da23e8228961b95af404c0deaf6a654b
Mrzhu <zhb@szltech.com>
2020-08-17 20:51:54 +0800

    数组方法原生实现

--- 自学数组方法/index.js
+++ 自学数组方法/index.js
@@ -30,17 +30,17 @@ function findSu1 (num) {
 
 
 
-// console.log(`该数字${findSu(1) ? '是' : '不是'}素数`)
+// console.log(`该数字${findSu(191) ? '是' : '不是'}素数`)
 /**
  * 实现sort/map/foreach/filter
  */
 Array.prototype.sort1 = function (method) {
-    const fn = method || function (val1, val2) {
+    method = method || function (val1, val2) {
         return val1 - val2
     }
     for (let i = 0; i < this.length; i++) {
         for (let j = 0; j < this.length - i - 1; j++) {
-            if (fn(this[j], this[j + 1]) > 0) {
+            if (method(this[j], this[j + 1]) > 0) {
                 [this[j], this[j + 1]] = [this[j + 1], this[j]]
             }
         }
@@ -66,7 +66,90 @@ function babelSort (arr) {
     return arr
 }
 // console.log(babelSort([1,3,5,0,2,-1]))
-const arr = [9,2,4,10,-1]
+// const arr = [9,2,4,10,-1]
 
 
-console.log(arr.sort1((num1, num2) => num2 - num1))
+// console.log(arr.sort1((num1, num2) => num2 - num1))
+
+/**
+ * map方法
+ */
+Array.prototype.map1 = function (fn) {
+    fn = fn || function (item) { return item }
+    const arr = []
+    for (let i = 0; i < this.length; i++) {
+        arr.push(fn(this[i], i, this))
+    }
+    return arr
+}
+const arr_test = [
+    {id: 1, name: '张三'},
+    {id: 2, name: '李四'},
+    {id: 3, name: '王五'}
+]
+const arr_num = [1, 3, -1, 100, 2000]
+// console.log(arr_test.map1((item, i) => {
+//     item.id = i
+//     return item
+// }))
+
+/**
+ * foreach
+ */
+Array.prototype.forEach1 = function (fn) {
+    fn = fn || function (item) { return item }
+    const arr = [...this]
+    for (let i = 0; i< arr.length; i++) {
+        fn(arr[i], i, arr)
+    }
+}
+// arr_test.forEach1((item, i) => {
+//     item.id = i
+//     item.name = ''
+// })
+// console.log('测试foreach', arr_test)
+
+/**
+ * filter
+ */
+Array.prototype.filter1 = function (fn) {
+    fn = fn || function (item) { return item }
+    const arr = []
+    for (let i = 0; i < this.length; i++) {
+        if(fn(this[i], i, this)) {
+            arr.push(this[i])
+        }
+    }
+    return arr
+}
+// console.log('filter', arr_num.filter1((item, i) => {
+//     return item > i
+// }))
+
+/**
+ * some
+ */
+Array.prototype.some1 = function (fn) {
+    fn = fn || function (item) { return item }
+    for (let i = 0; i < this.length; i++) {
+        if (fn(this[i], i, this)) return true
+    }
+    return false
+}
+// console.log('some', arr_num.some1(item => {
+//     return item < 0
+// }))
+
+/**
+ * every
+ */
+Array.prototype.every1 = function (fn) {
+    fn = fn || function (item) { return item }
+    for (let i = 0; i < this.length; i++) {
+        if(!fn(this[i], i, this)) return false
+    }
+    return true
+}
+// console.log('every', arr_num.every1(item => {
+//     return item > 1
+// }))
